fd424ac0e2d51ecd7a194817a5ee2359adfa8f66
Lucas Pereira <lpslucas27@gmail.com>
2024-12-03 19:05:10 -0300

    Creating model layer

---
+++ src/main/java/com/todolist/todolist/models/Task.java
@@ -0,0 +1,116 @@
+package com.todolist.todolist.models;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+    private String description;
+    private String status;
+    private String priority;
+    private Date dateCreation;
+    private Date dateConclusion;
+
+    public Task() {
+    }
+
+    public Task(String title, long id, String description, String status, String priority,
+                Date dateCreation, Date dateConclusion) {
+        this.title = title;
+        this.id = id;
+        this.description = description;
+        this.status = status;
+        this.priority = priority;
+        this.dateCreation = dateCreation;
+        this.dateConclusion = dateConclusion;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public Date getDateCreation() {
+        return dateCreation;
+    }
+
+    public void setDateCreation(Date dateCreation) {
+        this.dateCreation = dateCreation;
+    }
+
+    public Date getDateConclusion() {
+        return dateConclusion;
+    }
+
+    public void setDateConclusion(Date dateConclusion) {
+        this.dateConclusion = dateConclusion;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", status='" + status + '\'' +
+                ", priority='" + priority + '\'' +
+                ", dateCreation=" + dateCreation +
+                ", dateConclusion=" + dateConclusion +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Task task)) return false;
+        return id == task.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
